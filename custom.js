05c5cf1c9779f3996aa3d892322d7c885089c8d0
Rakesh Prakash <Rakesh Prakash>
2016-07-07 23:46:20 +0530

    removed jquery dependency, removed input type checkbox

--- custom.js
+++ custom.js
@@ -1,37 +1,51 @@
-$(function () {
-	var selectedVal = {};
-	$('.multiDropDownSelect, .multicaret').on('click', function () {
-		$('.multiDropDown').toggle('fast');
-	})
-	$('.multiDropDown li, input[type=checkbox]').on('click', function (e) {
-		var lbl = $(this).find("label").text() || $(this).siblings("label").text();
-		//stop duplicate trigger
-		e.stopImmediatePropagation();
-		//get the current checkbox
-		var currCB = $(this).find('input[type=checkbox]');
-		//toggle the checkbox tick
-		if (currCB.is(":checked")) {
-			currCB.prop('checked', false);
-		} else {
-			currCB.prop('checked', true);
-		}
-		updateSelectedValues(lbl);
-	});
+var selectedVal = {};
 
-	function updateSelectedValues(val) {
-		var html = "";
-		if (selectedVal["" + val]) {
-			delete selectedVal["" + val];
-		} else {
-			selectedVal["" + val] = val;
-		}
-		if (Object.keys(selectedVal).length) {
-			for (key in selectedVal) {
-				html += key + ",";
-			}
-			$('.multiDropDownSelect li').text(html.replace(/,\s*$/, ""));
-		} else {
-			$('.multiDropDownSelect li').text("Select");
+function showHideOptions() {
+	if (window.getComputedStyle(document.querySelectorAll('.multiDropDown')[0]).display == "none") {
+		document.querySelectorAll('.multiDropDown')[0].style.display = "block";
+	} else {
+		document.querySelectorAll('.multiDropDown')[0].style.display = "none";
+	}
+}
+
+
+(function () {
+	var selectElement = document.querySelectorAll('.multiDropDownSelect');
+	var caretElem = document.createElement('span');
+	caretElem.setAttribute('class', 'multicaret');
+	for (var i = 0; i < selectElement.length; i++) {
+		selectElement[i].parentElement.insertBefore(caretElem, selectElement[i]);
+		selectElement[i].addEventListener('click', showHideOptions);
+
+		var liArray = selectElement[i].nextElementSibling.children;
+		for (var j = 0; j < liArray.length; j++) {
+			liArray[j].className = "unchecked"
+			liArray[j].addEventListener('click', processClick);
 		}
 	}
-});
+})();
+
+function processClick() {
+	var lbl = this.children[0].innerHTML;
+	updateSelectedValues(lbl, this);
+}
+
+function updateSelectedValues(val, elm) {
+	var html = "";
+	if (selectedVal["" + val]) {
+		delete selectedVal["" + val];
+		elm.className = "unchecked";
+	} else {
+		selectedVal["" + val] = val;
+		elm.className = "checked";
+	}
+	debugger
+	if (Object.keys(selectedVal).length) {
+		for (key in selectedVal) {
+			html += key + ",";
+		}
+		elm.parentElement.previousElementSibling.children[0].innerHTML = html.replace(/,\s*$/, "");
+	} else {
+		elm.parentElement.previousElementSibling.children[0].innerHTML = "Select";
+	}
+}
